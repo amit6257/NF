f3e2ce125ba6b655fdcaba5be79f0342f385b46a
AMIT AGARWAL <amitagarwal@amits-mbp.hsd1.wa.comcast.net>
2016-07-13 01:13:52 -0700

    Initial Commit

---
+++ Thoughts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thoughts
+//
+//  Created by AMIT AGARWAL on 7/13/16.
+//  Copyright © 2016 AMIT AGARWAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thoughts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thoughts
+//
+//  Created by AMIT AGARWAL on 7/13/16.
+//  Copyright © 2016 AMIT AGARWAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
